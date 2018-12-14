29332e057d4b898e1b043b997915642d1a749f1f
jeff-gosselin <jgoz.email@gmail.com>
2018-12-14 14:13:50 -0500

    finished objects-belong to

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Song
+  attr_accessor :title, :artist
+  # @@all = []
+  #
+  # def initialize(title, artist)
+  #   @title = title
+  #   @artist = artist
+  #   @@all << self
+  # end
+  #
+  # def self.all
+  #   @@all
+  # end
+  #
+  #
+  # def artist_name
+  #   binding.pry
+  #   @artist
+  # end
+
+end
